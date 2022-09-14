28307263dbe6ffd7d1de33b0348e916ce806bb29
aysiane <aysianesarah@gmail.com>
2022-09-14 14:47:11 -0300

    Atualização

--- src/components/ListarCursos/ListarCursos.jsx
+++ src/components/ListarCursos/ListarCursos.jsx
@@ -1,15 +1,18 @@
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 import "./ListarCursos.css";
-import {CoursesContext} from "../CreateCourses/CoursesContext";
+import { CoursesContext } from "../CreateCourses/CoursesContext";
 
 export default function ListarCursos() {
   const { list } = useContext(CoursesContext);
   const courses = list();
+
+  
   console.log(courses)
 
   return (
     <div className="my-courses">
       <h2 className="section-title">Listar Cursos</h2>
+
 
     </div>
   )
