07b015e35063a62b0ad15f87defeadadca5ad919
wenderinacio <eduinacius@gmail.com>
2022-09-14 22:55:59 -0300

    V1

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:3001",
+  baseURL: "https://easystudy-api.herokuapp.com/",
 });
 
 export const apiCursos = axios.create({
